5099f254c09061dd28476c9694be2450730c7764
edina <edina111@gmail.com>
2016-05-06 13:35:40 +0300

    helper function moved to main as the 3rd for
    I got it now, tough with the helper seamed to be smoother :)

--- matrix.cpp
+++ matrix.cpp
@@ -8,13 +8,7 @@ using namespace std;
 // Matrix 2nd : 1 2 3 3 2 1 4 5 2
 // --------------------------------
 // Result      19 21 11 43 48 29 35 35 31
-
-
-
- const  int m1[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 1};
- const  int m2[3][3] = {1, 2, 3, 3, 2, 1, 4, 5, 2};
  
-
 void printMatrix( int m[3][3])
 {
      for (int i=0; i< 3; i++)
@@ -28,33 +22,27 @@ void printMatrix( int m[3][3])
  
 }
 
-int sumofRawColoum(int raw, int coloum)
-{
-  int sum = 0;
-  
-  for (int j = 0; j< 3; j++)
-  {
-    sum += m1[raw][j] * m2[j][coloum];
-  }
-  return sum;
-}
-
 int main()
 {
-  //int m1[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 1};
-  //int m2[3][3] = {1, 2, 3, 3, 2, 1, 4, 5, 2};
-  
-  int result[3][3] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-          
-  for (int i=0; i< 3; i++)
-    {   
+    int m1[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 1};
+    int m2[3][3] = {1, 2, 3, 3, 2, 1, 4, 5, 2};
+    int result[3][3] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    
+    // the first and second for represents the going through result matrix
+    // the third for calculates the SUM of the respectiv raw/coloumn
+    for (int i=0; i< 3; i++)
+    {
         for (int j=0; j< 3; j++)
         {
-            result [i][j] = sumofRawColoum(i, j);
+            int sum = 0;
+            for (int k = 0; k< 3; k++)
+            {
+                sum += m1[i][k] * m2[k][j];
+            }
+            result [i][j] = sum;
         }
     }
- 
- printMatrix(result);
-    
-  cout << "test2" << endl;
+  
+  printMatrix(result);
+  
 }
